4cf2f826f82c420f872ef9c5888978a0b6b65afa
Priscila <priscila.campoos@yahoo.com.br>
2024-07-27 18:50:08 +0100

    Validação de formulario e disparo de toast

---
+++ JS/meujs.js
@@ -0,0 +1,18 @@
+document.addEventListener('DOMContentLoaded', function() {
+  const form = document.getElementById('formulario-contato');
+  const button = document.getElementById('enviarToastBtn');
+  const toast = new bootstrap.Toast(document.getElementById('enviarToast'));
+
+  form.addEventListener('input', function() {
+    button.disabled = !form.checkValidity();
+  });
+
+  form.addEventListener('submit', function(event) {
+    event.preventDefault();
+    if (form.checkValidity()) {
+      toast.show();
+    } else {
+      form.reportValidity();
+    }
+  });
+});
